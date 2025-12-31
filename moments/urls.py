4c519bb015bb59b9564b033d9ace7509c8137f13
Phuvanenthran-P <phuvan0205@gmail.com>
2025-12-31 12:22:27 +0530

    Secure update and delete for user-owned moments

--- moments/urls.py
+++ moments/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.moment_list, name="moment_list"),
     path("create/", views.moment_create, name="moment_create"),
+    path("<int:pk>/edit/", views.moment_update, name="moment_update"),
+    path("<int:pk>/delete/", views.moment_delete, name="moment_delete"),
 ]
+
--- moments/views.py
+++ moments/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Moment
 from .forms import MomentForm
 
@@ -23,3 +23,29 @@ def moment_create(request):
         form = MomentForm()
 
     return render(request, "moments/moment_form.html", {"form": form})
+
+
+@login_required
+def moment_update(request, pk):
+    moment = get_object_or_404(Moment, pk=pk, owner=request.user)
+
+    if request.method == "POST":
+        form = MomentForm(request.POST, request.FILES, instance=moment)
+        if form.is_valid():
+            form.save()
+            return redirect("moment_list")
+    else:
+        form = MomentForm(instance=moment)
+
+    return render(request, "moments/moment_form.html", {"form": form})
+
+
+@login_required
+def moment_delete(request, pk):
+    moment = get_object_or_404(Moment, pk=pk, owner=request.user)
+
+    if request.method == "POST":
+        moment.delete()
+        return redirect("moment_list")
+
+    return render(request, "moments/moment_confirm_delete.html", {"moment": moment})
